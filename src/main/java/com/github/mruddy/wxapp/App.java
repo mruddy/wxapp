3cb38ca0e5f8e32b8f885475938c57190715007e
mruddy <mruddy@users.noreply.github.com>
2023-01-28 08:39:54 -0500

    ensure that the temp file and the output file are on the same file system so that the move is atomic

--- src/main/java/com/github/mruddy/wxapp/App.java
+++ src/main/java/com/github/mruddy/wxapp/App.java
@@ -26,18 +26,22 @@ public class App {
   public static void main(final String[] args) throws Exception {
     final int wxStationSourcePort = Integer.parseInt(System.getenv("WXSTATION_SOURCE_PORT"), 10);
     final InetSocketAddress wxStationSource = new InetSocketAddress(System.getenv("WXSTATION_SOURCE_HOST"), wxStationSourcePort);
-    final Path outputPath = Path.of(System.getenv("WXAPP_OUTPUT_PATH"));
-    App.log.info(String.format("starting: wx = %s output = %s", wxStationSource, outputPath));
+    final Path outputFile = Path.of(System.getenv("WXAPP_OUTPUT_FILE")).toRealPath();
+    final Path outputDir = outputFile.getParent();
+    App.log.info(String.format("starting: wx = %s output file = %s output dir = %s", wxStationSource, outputFile, outputDir));
     final BlockingQueue<Weather> messsageQueue = new LinkedBlockingQueue<>();
     final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     scheduledExecutorService.scheduleWithFixedDelay(new WxClientTask(wxStationSource, messsageQueue), 0, 1, TimeUnit.SECONDS);
     while (true) {
-      final String message = messsageQueue.take().toString();
-      final Path tempFile = Files.createTempFile(null, null, PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-r--r--")));
-      App.log.fine(String.format("temp = %s", tempFile));
-      Files.write(tempFile, message.getBytes(StandardCharsets.UTF_8));
-      Files.move(tempFile, outputPath, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
-      App.log.fine(message);
+      try {
+        final String message = messsageQueue.take().toString();
+        App.log.fine(message);
+        final Path tempFile = Files.createTempFile(outputDir, null, null, PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-r--r--")));
+        Files.write(tempFile, message.getBytes(StandardCharsets.UTF_8));
+        Files.move(tempFile, outputFile, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
+      } catch (final Exception e) {
+        App.log.info(e.toString());
+      }
     }
   }
 }
